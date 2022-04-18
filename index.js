1c246837099b58de9e68827d2e2f11d0cfcf8c9a
vardhan2000 <vardhan.av2000@gmail.com>
2022-04-18 19:34:58 +0530

    select shape and translate along any axis

--- index.js
+++ index.js
@@ -10,50 +10,25 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
 
-// const loader = new GLTFLoader();
+let canvas = renderer.domElement;
 
-// function load_meshObj(file){
-//     let obj;
-//     loader.load(
-//         // resource URL
-//         file,
-//         // called when the resource is loaded
-//         function ( gltf ) {
-//             obj = gltf.asset;
-//             scene.add( gltf.scene );
-//             // gltf.asset;
-    
-//         },
-//         // called while loading is progressing
-//         function ( xhr ) {
-    
-//             console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-    
-//         },
-//         // called when loading has errors
-//         function ( error ) {
-    
-//             console.log( 'An error happened' );
-    
-//         }
-//     ); 
-    
-//     return obj;
-// }
+document.body.appendChild( canvas );
 
-// let sphere = load_meshObj('./sphere.glb');
-
-// load_meshObj('./sphere.glb');
-
-// sphere.material.color.setHex(0x00ff00);
 
 const loader = new OBJLoader();
-let obj;
 // load a resource
 
-function loadMeshObj(file, objName){
+// function hex2rgba (hex, alpha = 1) {
+// 	const [r, g, b] = hex.match(/\w\w/g).map(x => parseInt(x, 16));
+// 	return `rgba(${r},${g},${b},${alpha})`;
+//   };
+
+// console.log(hex2rgba(0x00ff00));
+
+let primitives = 0;
+
+function loadMeshObj(file, objID, objColor){
 
 	loader.load(
 		// resource URL
@@ -63,52 +38,177 @@ function loadMeshObj(file, objName){
 			object.traverse( function ( obj ) {
 				if ( obj.isMesh ) {
 				  //  obj.material.ambient.set(0xFF0000);
-					obj.material.color.setHex( 0x00ff00 );
+					obj.material.color.setHex( objColor );
+
+					// console.log(obj.material.color['g']);
 					// obj.material.needsUpdate = true;
-					console.log(obj)
+					// console.log(obj)
 				}
 			});
-			object.name = objName;
-			object.position['x']+=2;
+			console.log(object);
+			object.name = objID;
+			// object.position['x']+=2;
 			// console.log();
 			// object.translateOnAxis(THREE.Vector3(1,0,0),10);
 			scene.add( object );
+			primitives += 1;
 	
 		},
 		// called when loading is in progresses
-		function ( xhr ) {
+		// function ( xhr ) {
 	
-			console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+		// 	console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
 	
-		},
-		// called when loading has errors
-		function ( error ) {
+		// },
+		// // called when loading has errors
+		// function ( error ) {
 	
-			console.log( 'An error happened' );
+		// 	console.log( 'An error happened' );
 	
-		}
+		// }
 	);
 
 }
 
-loadMeshObj('./cube.obj','cube');
+loadMeshObj('./cube.obj',(primitives+3).toString(), 0x00ff00);
 
-// console.log(scene.getObjectByName("sphere").isObject3D());
 
 scene.add(new THREE.AmbientLight(0xffffff))
 
-// const controls = new TrackballControls(scene.getObjectByName("cube"), renderer.domElement);
-
-
-// let color = new THREE.Color( 0xFFB6C1 );
-
-// obj.traverse( function (obj) {
-// 	if (obj.isMesh){
-// 	  obj.material.color.set(0xFFB6C1);
-// 	}
-//   } );
-
 camera.position.z = 5;
+
+let mode = "none";
+let selectedShape = null;
+let moveBy = 0.05;
+
+document.addEventListener('keydown', function (event) {
+	console.log("Key pressed = ", event.key);
+
+	if(event.key=="m"){
+		mode = "m";	
+	}
+
+	else if(event.key=="i") {
+		mode = "i";
+	}
+
+	else if(event.key == "x" && mode=="m"){
+		selectedShape.position['x']-=moveBy;
+	}
+
+	else if(event.key == "X" && mode=="m"){
+		selectedShape.position['x']+=moveBy;
+	}
+
+	else if(event.key == "y" && mode=="m") {
+		selectedShape.position['y']-=moveBy;
+	}
+
+	else if(event.key=="Y" && mode=="m") {
+		selectedShape.position['y']+=moveBy;
+	}
+
+	else if(event.key=="z" && mode=="m"){
+		selectedShape.position['z']-=moveBy;
+	}
+
+	else if(event.key=="Z" && mode=="m"){
+		selectedShape.position['z']+=moveBy;
+	}
+
+}, false );
+
+let mouseX, mouseY;
+let gl = renderer.getContext();
+let pixelColor = new Uint8Array(4);
+
+function colorDiff(color1, color2){
+	let dist=0
+	for(let i=0; i<color1.length; i++){
+		dist += (color1[i]-color2[i])*(color1[i]-color2[i])
+	}
+	return Math.sqrt(dist)
+}
+
+function selectShape(pixelColor)
+{
+	let pixelCol = new Float32Array(pixelColor);
+	let color = pixelCol.map(function(x) {return x/255.0;})
+
+	let shapeID = "2";
+	if(colorDiff([0.9,0.9,0.9,1],color) < 0.002){
+		return shapeID;
+	}
+
+	for(let i=0;i<primitives;i++)
+	{
+		// if(!primitives[i].selectable)
+		// 	continue;
+		let object = scene.getObjectByName((i+3).toString());
+
+		object.traverse( function ( obj ) {
+			if ( obj.isMesh ) {
+				let objCol = [obj.material.color['r'],obj.material.color['g'],obj.material.color['b'],1];
+				if(colorDiff(objCol,color)<0.002){
+					shapeID = object.name;
+					// return shapeID;
+				}
+				console.log(obj)
+			}
+		});
+
+		// if(colorDiff(primitives[i].color,color) < 0.002){
+		// 	shapeID = primitives[i]
+		// }
+	}
+	console.log("shapeID = ",shapeID);
+	return shapeID;
+}
+
+canvas.addEventListener('mousedown', (event) => {
+	if(mode=="m"){
+		// const rect = renderer.getDomElement().getBoundingClientRect();
+		const rect = canvas.getBoundingClientRect();
+
+		mouseX = event.clientX - rect.left;
+		mouseY = event.clientY - rect.top;
+
+		const pixelX = mouseX * gl.canvas.width / gl.canvas.clientWidth;
+		const pixelY = gl.canvas.height - mouseY * gl.canvas.height / gl.canvas.clientHeight - 1;
+
+		// renderer.render(scene,shader);
+		renderer.render( scene, camera );
+
+		gl.readPixels(
+			pixelX,            // x
+			pixelY,            // y
+			1,                 // width
+			1,                 // height
+			gl.RGBA,           // format
+			gl.UNSIGNED_BYTE,  // type
+			pixelColor         // typed array to hold result
+		);
+
+		// console.log("pixelColor = ", pixelColor);
+
+		// let s = currentShape;
+		let shape_id = selectShape(pixelColor);
+		
+		console.log(shape_id)
+
+		// if(s != null)
+		// 	s.color = s.originalColor;
+
+		if(shape_id == "2"){
+			console.log("No shape selected");
+		}
+		else {
+			selectedShape = scene.getObjectByName(shape_id);
+			// console.log(selectedShape);
+		}
+	}
+});
+
 
 function animate() {
     requestAnimationFrame( animate );
