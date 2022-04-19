fc1d11481bbbc123c7a8aa77a5a9ba8cf495c676
architsangal <architsangal2000@gmail.com>
2022-04-19 23:28:11 +0530

    remove useless comments and implemented

--- index.js
+++ index.js
@@ -31,32 +31,16 @@ function loadMeshObj(file, objID, objColor) {
 			object.traverse(function (obj) {
 				if (obj.isMesh) {
 					obj.material.color.setHex(objColor);
-					
-					console.log(obj.material);
 				}
 			});
-			console.log("isObject3D = ", object.isObject3D);
+
 			object.name = objID;
 			
 			scene.add(object);
 			bbox = new THREE.Box3().setFromObject(scene.getObjectByName(objID))
-			console.log(bbox)
-			// primitives += 1;
-			console.log(`primitives = ${primitives}`)
+
 
 		},
-		// called when loading is in progresses
-		// function (xhr) {
-
-		// 	console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-
-		// },
-		// // called when loading has errors
-		// function (error) {
-
-		// 	console.log('An error happened');
-
-		// }
 	);
 	primitives += 1;
 }
@@ -80,30 +64,22 @@ function computeLimits(bbox) {
 }
 
 loadMeshObj('./cube.obj', (primitives + 3).toString(), 0x00ff00);
-// console.log("objID = ", primitives + 3) 
 loadMeshObj('./CG_assn3.obj', (primitives + 3).toString(), 0xff0000);
-// console.log("objID = ", primitives + 3)
 
 let light = new THREE.AmbientLight(0xffffff)
 light.name = "light"
 scene.add(light)
 scene.getObjectByName("light").visible = false;
 
-
-// let bbox = new THREE.Box3().setFromObject(scene.getObjectByName("3"));
-// scene.add(new THREE.AmbientLight(0xffffff))
-
-const l3 = new THREE.PointLight( 0xffffff, 1, 100 );
+const l3 = new THREE.PointLight( 0xffffff, 1, 100);
 l3.position.set( 0, 0, 0 );
 l3.name = "l3";
 scene.add( l3 );
-console.log(l3)
 
 const l4 = new THREE.PointLight( 0xffffff, 1, 100 );
 l4.position.set( 0, 0, 0 );
 l4.name = "l4";
 scene.add( l4 );
-console.log(l4.isLight)
 
 camera.position.z = 5;
 
@@ -111,9 +87,6 @@ let mode = "none";
 let selectedShape = null;
 let moveBy = 0.05;
 let offset = 3;
-
-const rotationMatrix = new THREE.Matrix4();
-const targetQuaternion = new THREE.Quaternion();
 
 document.addEventListener('keydown', function (event) {
 	console.log("Key pressed = ", event.key);
@@ -123,10 +96,7 @@ document.addEventListener('keydown', function (event) {
 		if(selectedShape!=null){
 			bbox = new THREE.Box3().setFromObject(selectedShape)
 			limits = computeLimits(bbox)
-			// console.log(bbox)
 		}
-		
-		// console.log(limits)
 	}
 
 	if (event.key == "m") {
@@ -137,25 +107,18 @@ document.addEventListener('keydown', function (event) {
 	else if(event.key == "s"){
 
 		selectedShape.traverse(function (obj) {
-			console.log("obj.isMesh = ", obj.isMesh)
 			if (obj.isMesh) {
 				let col = obj.material.color
-				// obj.material.dispose()
-				// console.log("obj.isMeshPhongMaterial = ", obj.isMeshPhongMaterial)
-				// console.log(obj.material.type = 'MeshPhongMaterial')
 				if(obj.material.type == 'MeshPhongMaterial'){
 					obj.material.dispose()
 					obj.material = new THREE.MeshLambertMaterial()
-					console.log("hello")
 				} else {
 					obj.material.dispose()
 					obj.material = new THREE.MeshPhongMaterial()
-					console.log("world")
 				}
 				obj.material.needsUpdate = true;
 				
 				obj.material.color = col;
-				console.log(obj.material)
 			}
 		});
 		
@@ -166,7 +129,7 @@ document.addEventListener('keydown', function (event) {
 		mode = "i";
 	}
 
-	else if (event.key == "x") {
+	else if (event.key == "a") {
 		let lightName = "l" + selectedShape.name
 		if(mode == "m"){
 			selectedShape.position['x'] -= moveBy;
@@ -177,11 +140,9 @@ document.addEventListener('keydown', function (event) {
 				scene.getObjectByName(lightName).position['x'] -= moveBy; 
 			}
 		}
-		
-		// console.log(scene.getObjectByName(lightName).position)
 	}
 
-	else if (event.key == "X") {
+	else if (event.key == "d") {
 		let lightName = "l" + selectedShape.name
 		if(mode == "m"){
 			selectedShape.position['x'] += moveBy;
@@ -192,11 +153,9 @@ document.addEventListener('keydown', function (event) {
 				scene.getObjectByName(lightName).position['x'] += moveBy; 
 			}
 		}
-
-		console.log(scene.getObjectByName(lightName).position)
 	}
 
-	else if (event.key == "y") {
+	else if (event.key == "w") {
 		let lightName = "l" + selectedShape.name
 		if(mode == "m") {
 			selectedShape.position['y'] -= moveBy;
@@ -209,7 +168,7 @@ document.addEventListener('keydown', function (event) {
 		}
 	}
 
-	else if (event.key == "Y") {
+	else if (event.key == "s") {
 		let lightName = "l" + selectedShape.name
 		if(mode == "m") {
 			selectedShape.position['y'] += moveBy;
@@ -235,7 +194,7 @@ document.addEventListener('keydown', function (event) {
 		}
 	}
 
-	else if (event.key == "Z") {
+	else if (event.key == "x") {
 		let lightName = "l" + selectedShape.name
 		if(mode == "m") {
 			selectedShape.position['z'] += moveBy;
@@ -262,124 +221,50 @@ document.addEventListener('keydown', function (event) {
 		selectedShape = scene.getObjectByName(event.key)
 	}
 
-	else if (event.key == "(") {
-		rotationMatrix.lookAt(new THREE.Vector3(0,0,1), selectedShape.position, selectedShape.up );
-		console.log(rotationMatrix)
-		targetQuaternion.setFromRotationMatrix(rotationMatrix);
-		selectedShape.quaternion.rotateTowards(targetQuaternion, 0.05).normalize();
+	else if (event.key == "o")
+	{
+		const quaternion = new THREE.Quaternion();
+		quaternion.setFromAxisAngle( new THREE.Vector3( 0, 1, 0 ), Math.PI / 180 );
+		selectedShape.applyQuaternion( quaternion );
+	}
+
+	else if (event.key == "p")
+	{
+		const quaternion = new THREE.Quaternion();
+		quaternion.setFromAxisAngle( new THREE.Vector3( 0, 1, 0 ), -Math.PI / 180 );
+		selectedShape.applyQuaternion( quaternion );
+	}
+
+	else if (event.key == "k")
+	{
+		const quaternion = new THREE.Quaternion();
+		quaternion.setFromAxisAngle( new THREE.Vector3( 0, 1, 0 ), Math.PI / 180 );
+		selectedShape.applyQuaternion( quaternion );
+	}
+
+	else if (event.key == "l")
+	{
+		const quaternion = new THREE.Quaternion();
+		quaternion.setFromAxisAngle( new THREE.Vector3( 0, 1, 0 ), -Math.PI / 180 );
+		selectedShape.applyQuaternion( quaternion );
+	}
+
+	else if (event.key == "n")
+	{
+		const quaternion = new THREE.Quaternion();
+		quaternion.setFromAxisAngle( new THREE.Vector3( 0, 1, 0 ), Math.PI / 180 );
+		selectedShape.applyQuaternion( quaternion );
+	}
+
+	else if (event.key == "m")
+	{
+		const quaternion = new THREE.Quaternion();
+		quaternion.setFromAxisAngle( new THREE.Vector3( 0, 1, 0 ), -Math.PI / 180 );
+		selectedShape.applyQuaternion( quaternion );
 	}
 
 
-
 }, false);
-
-// let mouseX, mouseY;
-// let gl = renderer.getContext();
-// let pixelColor = new Uint8Array(4);
-
-// function colorDiff(color1, color2) {
-// 	let dist = 0
-// 	for (let i = 0; i < color1.length; i++) {
-// 		dist += (color1[i] - color2[i]) * (color1[i] - color2[i])
-// 	}
-// 	return Math.sqrt(dist)
-// }
-
-// function selectShape(pixelColor) {
-// 	// scene.getObjectByName("light").visible = true;
-// 	let pixelCol = new Float32Array(pixelColor);
-// 	let color = pixelCol.map(function (x) { return x / 255.0; })
-
-// 	let shapeID = "2";
-// 	if (colorDiff([0.9, 0.9, 0.9, 1], color) < 0.002) {
-// 		return shapeID;
-// 	}
-
-// 	for (let i = 0; i < primitives; i++) {
-// 		// if(!primitives[i].selectable)
-// 		// 	continue;
-// 		// if()
-
-// 		let object = scene.getObjectByName((i + 3).toString());
-
-// 		object.traverse(function (obj) {
-// 			if (obj.isMesh) {
-// 				let objCol = [obj.material.color['r'], obj.material.color['g'], obj.material.color['b'], 1];
-// 				if (colorDiff(objCol, color) < 0.002) {
-// 					shapeID = object.name;
-// 					// return shapeID;
-// 				}
-// 				console.log(obj)
-// 			}
-// 		});
-// 	}
-// 	console.log("shapeID = ", shapeID);
-// 	// scene.getObjectByName("light").visible = false;
-// 	return shapeID;
-// }
-
-// canvas.addEventListener('mousedown', (event) => {
-// 	if (mode == "m" || mode == "i") {
-// 		scene.getObjectByName("light").visible = true;
-
-// 		for (let i = 0; i < primitives; i++) {
-// 			let lightName = "l" + (i + 3).toString()
-// 			scene.getObjectByName(lightName).visible = false;
-// 		}
-
-// 		// scene.getObjectByName("l1").visible = false;
-// 		// const rect = renderer.getDomElement().getBoundingClientRect();
-// 		const rect = canvas.getBoundingClientRect();
-
-// 		mouseX = event.clientX - rect.left;
-// 		mouseY = event.clientY - rect.top;
-
-// 		// console.log(`mouseX = ${mouseX}; mouseY = ${mouseY}}`)
-
-// 		const pixelX = mouseX * gl.canvas.width / gl.canvas.clientWidth;
-// 		const pixelY = gl.canvas.height - mouseY * gl.canvas.height / gl.canvas.clientHeight - 1;
-
-// 		// console.log(`pixelX = ${pixelX}; pixelY = ${pixelY}}`)
-
-// 		// renderer.render(scene,shader);
-// 		renderer.render(scene, camera);
-
-// 		gl.readPixels(
-// 			pixelX,            // x
-// 			pixelY,            // y
-// 			1,                 // width
-// 			1,                 // height
-// 			gl.RGBA,           // format
-// 			gl.UNSIGNED_BYTE,  // type
-// 			pixelColor         // typed array to hold result
-// 		);
-
-// 		// console.log("pixelColor = ", pixelColor);
-
-// 		// let s = currentShape;
-// 		let shape_id = selectShape(pixelColor);
-
-// 		scene.getObjectByName("light").visible = false;
-		
-// 		for (let i = 0; i < primitives; i++) {
-// 			let lightName = "l" + (i + 3).toString()
-// 			scene.getObjectByName(lightName).visible = true;
-// 		}
-		
-// 		console.log(shape_id)
-
-// 		// if(s != null)
-// 		// 	s.color = s.originalColor;
-
-// 		if (shape_id == "2") {
-// 			console.log("No shape selected");
-// 		}
-// 		else {
-// 			selectedShape = scene.getObjectByName(shape_id);
-// 			// console.log(selectedShape);
-// 		}
-// 	} 
-// });
 
 function animate() {
 	requestAnimationFrame(animate);
