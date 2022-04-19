aee89891d558ccd07f1c3a793d73bc551da27e5b
architsangal <architsangal2000@gmail.com>
2022-04-19 18:35:59 +0530

    Key Bindings for Selecting Objects

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ import * as THREE from 'three'
 import { OBJLoader } from 'https://unpkg.com/three/examples/jsm/loaders/OBJLoader.js';
 // import TrackballControls from 'https://cdn.jsdelivr.net/npm/three-trackballcontrols@0.0.8/index.min.js';
 
-
 const scene = new THREE.Scene();
+
+// TODO comment the below
 scene.background = new THREE.Color(0xe5e5e5);
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -14,7 +15,6 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 let canvas = renderer.domElement;
 
 document.body.appendChild(canvas);
-
 
 const loader = new OBJLoader();
 
@@ -31,7 +31,8 @@ function loadMeshObj(file, objID, objColor) {
 			object.traverse(function (obj) {
 				if (obj.isMesh) {
 					obj.material.color.setHex(objColor);
-					console.log(obj.material)
+					
+					console.log(obj.material);
 				}
 			});
 			console.log("isObject3D = ", object.isObject3D);
@@ -257,6 +258,10 @@ document.addEventListener('keydown', function (event) {
 		scene.getObjectByName(lightName).visible = true;
 	}
 
+	else if(Number.isInteger(Number(event.key)) && Number(event.key)>2 && (mode == "m" || mode == "i")) {
+		selectedShape = scene.getObjectByName(event.key)
+	}
+
 	else if (event.key == "(") {
 		rotationMatrix.lookAt(new THREE.Vector3(0,0,1), selectedShape.position, selectedShape.up );
 		console.log(rotationMatrix)
@@ -268,114 +273,113 @@ document.addEventListener('keydown', function (event) {
 
 }, false);
 
-let mouseX, mouseY;
-let gl = renderer.getContext();
-let pixelColor = new Uint8Array(4);
+// let mouseX, mouseY;
+// let gl = renderer.getContext();
+// let pixelColor = new Uint8Array(4);
 
-function colorDiff(color1, color2) {
-	let dist = 0
-	for (let i = 0; i < color1.length; i++) {
-		dist += (color1[i] - color2[i]) * (color1[i] - color2[i])
-	}
-	return Math.sqrt(dist)
-}
+// function colorDiff(color1, color2) {
+// 	let dist = 0
+// 	for (let i = 0; i < color1.length; i++) {
+// 		dist += (color1[i] - color2[i]) * (color1[i] - color2[i])
+// 	}
+// 	return Math.sqrt(dist)
+// }
 
-function selectShape(pixelColor) {
-	// scene.getObjectByName("light").visible = true;
-	let pixelCol = new Float32Array(pixelColor);
-	let color = pixelCol.map(function (x) { return x / 255.0; })
+// function selectShape(pixelColor) {
+// 	// scene.getObjectByName("light").visible = true;
+// 	let pixelCol = new Float32Array(pixelColor);
+// 	let color = pixelCol.map(function (x) { return x / 255.0; })
 
-	let shapeID = "2";
-	if (colorDiff([0.9, 0.9, 0.9, 1], color) < 0.002) {
-		return shapeID;
-	}
+// 	let shapeID = "2";
+// 	if (colorDiff([0.9, 0.9, 0.9, 1], color) < 0.002) {
+// 		return shapeID;
+// 	}
 
-	for (let i = 0; i < primitives; i++) {
-		// if(!primitives[i].selectable)
-		// 	continue;
-		// if()
+// 	for (let i = 0; i < primitives; i++) {
+// 		// if(!primitives[i].selectable)
+// 		// 	continue;
+// 		// if()
 
-		let object = scene.getObjectByName((i + 3).toString());
+// 		let object = scene.getObjectByName((i + 3).toString());
 
-		object.traverse(function (obj) {
-			if (obj.isMesh) {
-				let objCol = [obj.material.color['r'], obj.material.color['g'], obj.material.color['b'], 1];
-				if (colorDiff(objCol, color) < 0.002) {
-					shapeID = object.name;
-					// return shapeID;
-				}
-				console.log(obj)
-			}
-		});
-	}
-	console.log("shapeID = ", shapeID);
-	// scene.getObjectByName("light").visible = false;
-	return shapeID;
-}
+// 		object.traverse(function (obj) {
+// 			if (obj.isMesh) {
+// 				let objCol = [obj.material.color['r'], obj.material.color['g'], obj.material.color['b'], 1];
+// 				if (colorDiff(objCol, color) < 0.002) {
+// 					shapeID = object.name;
+// 					// return shapeID;
+// 				}
+// 				console.log(obj)
+// 			}
+// 		});
+// 	}
+// 	console.log("shapeID = ", shapeID);
+// 	// scene.getObjectByName("light").visible = false;
+// 	return shapeID;
+// }
 
-canvas.addEventListener('mousedown', (event) => {
-	if (mode == "m" || mode == "i") {
-		scene.getObjectByName("light").visible = true;
+// canvas.addEventListener('mousedown', (event) => {
+// 	if (mode == "m" || mode == "i") {
+// 		scene.getObjectByName("light").visible = true;
 
-		for (let i = 0; i < primitives; i++) {
-			let lightName = "l" + (i + 3).toString()
-			scene.getObjectByName(lightName).visible = false;
-		}
+// 		for (let i = 0; i < primitives; i++) {
+// 			let lightName = "l" + (i + 3).toString()
+// 			scene.getObjectByName(lightName).visible = false;
+// 		}
 
-		// scene.getObjectByName("l1").visible = false;
-		// const rect = renderer.getDomElement().getBoundingClientRect();
-		const rect = canvas.getBoundingClientRect();
+// 		// scene.getObjectByName("l1").visible = false;
+// 		// const rect = renderer.getDomElement().getBoundingClientRect();
+// 		const rect = canvas.getBoundingClientRect();
 
-		mouseX = event.clientX - rect.left;
-		mouseY = event.clientY - rect.top;
+// 		mouseX = event.clientX - rect.left;
+// 		mouseY = event.clientY - rect.top;
 
-		// console.log(`mouseX = ${mouseX}; mouseY = ${mouseY}}`)
+// 		// console.log(`mouseX = ${mouseX}; mouseY = ${mouseY}}`)
 
-		const pixelX = mouseX * gl.canvas.width / gl.canvas.clientWidth;
-		const pixelY = gl.canvas.height - mouseY * gl.canvas.height / gl.canvas.clientHeight - 1;
+// 		const pixelX = mouseX * gl.canvas.width / gl.canvas.clientWidth;
+// 		const pixelY = gl.canvas.height - mouseY * gl.canvas.height / gl.canvas.clientHeight - 1;
 
-		// console.log(`pixelX = ${pixelX}; pixelY = ${pixelY}}`)
+// 		// console.log(`pixelX = ${pixelX}; pixelY = ${pixelY}}`)
 
-		// renderer.render(scene,shader);
-		renderer.render(scene, camera);
+// 		// renderer.render(scene,shader);
+// 		renderer.render(scene, camera);
 
-		gl.readPixels(
-			pixelX,            // x
-			pixelY,            // y
-			1,                 // width
-			1,                 // height
-			gl.RGBA,           // format
-			gl.UNSIGNED_BYTE,  // type
-			pixelColor         // typed array to hold result
-		);
+// 		gl.readPixels(
+// 			pixelX,            // x
+// 			pixelY,            // y
+// 			1,                 // width
+// 			1,                 // height
+// 			gl.RGBA,           // format
+// 			gl.UNSIGNED_BYTE,  // type
+// 			pixelColor         // typed array to hold result
+// 		);
 
-		// console.log("pixelColor = ", pixelColor);
+// 		// console.log("pixelColor = ", pixelColor);
 
-		// let s = currentShape;
-		let shape_id = selectShape(pixelColor);
+// 		// let s = currentShape;
+// 		let shape_id = selectShape(pixelColor);
 
-		scene.getObjectByName("light").visible = false;
+// 		scene.getObjectByName("light").visible = false;
 		
-		for (let i = 0; i < primitives; i++) {
-			let lightName = "l" + (i + 3).toString()
-			scene.getObjectByName(lightName).visible = true;
-		}
+// 		for (let i = 0; i < primitives; i++) {
+// 			let lightName = "l" + (i + 3).toString()
+// 			scene.getObjectByName(lightName).visible = true;
+// 		}
 		
-		console.log(shape_id)
+// 		console.log(shape_id)
 
-		// if(s != null)
-		// 	s.color = s.originalColor;
+// 		// if(s != null)
+// 		// 	s.color = s.originalColor;
 
-		if (shape_id == "2") {
-			console.log("No shape selected");
-		}
-		else {
-			selectedShape = scene.getObjectByName(shape_id);
-			// console.log(selectedShape);
-		}
-	} 
-});
-
+// 		if (shape_id == "2") {
+// 			console.log("No shape selected");
+// 		}
+// 		else {
+// 			selectedShape = scene.getObjectByName(shape_id);
+// 			// console.log(selectedShape);
+// 		}
+// 	} 
+// });
 
 function animate() {
 	requestAnimationFrame(animate);
